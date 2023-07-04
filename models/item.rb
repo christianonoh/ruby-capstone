94df9074c0cd2bd37466802ad7e14e22dec9d1a8
christianonoh <onohchibyk@gmail.com>
2023-07-04 16:47:36 +0100

    Add item class

---
+++ models/item.rb
@@ -0,0 +1,26 @@
+require 'date'
+
+class Item
+  attr_accessor :archived
+
+  def initialize(genre, author, label, publish_date, archived: false)
+    @id = Random.rand(1..10000)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end
+
+  def can_be_archived?
+    Date.today - @publish_date > (10 * 365)
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      self.archived = true
+    else
+      self.archived = false
+    end
+  end
+end
