fe29bff136902cedeeb7c522e7916a5a445d69cd
christianonoh <onohchibyk@gmail.com>
2023-07-04 19:32:38 +0100

    Add App class with dummy methods

---
+++ app.rb
@@ -0,0 +1,45 @@
+class App
+  def list_all_books
+    puts "Listing all books"
+  end
+
+  def list_all_music_albums
+    puts "Listing all music albums"
+  end
+
+  def list_all_games
+    puts "Listing all games"
+  end
+
+  def list_all_genres
+    puts "Listing all genres"
+  end
+
+  def list_all_labels
+    puts "Listing all labels"
+  end
+
+  def list_all_authors
+    puts "Listing all authors"
+  end
+
+  def add_book
+    puts "Adding a book"
+  end
+
+  def add_music_album
+    puts "Adding a music album"
+  end
+
+  def add_game
+    puts "Adding a game"
+  end
+
+  def add_genre
+    puts "Adding a genre"
+  end
+
+  def quit
+    puts "Goodbye!"
+  end
+end
