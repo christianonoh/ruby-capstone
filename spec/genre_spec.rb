9086632fc9b9bd227ef543a2d95509db76472dc9
Daniela Moreno <dani9292@hotmail.com>
2023-07-04 16:02:37 -0500

    fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,25 +1,25 @@
 require './models/genre'
 
 describe Genre do
-    let(:genre) { Genre.new("Rock") }
-        
-    describe '#new' do
-        it "returns a new Genre object" do
-        expect(genre).to be_an_instance_of(Genre)
-        end
+  let(:genre) { Genre.new('Rock') }
+
+  describe '#new' do
+    it 'returns a new Genre object' do
+      expect(genre).to be_an_instance_of(Genre)
     end
-    
-    describe '#name' do
-        it "returns the correct name" do
-        expect(genre.name).to eq("Rock")
-        end
+  end
+
+  describe '#name' do
+    it 'returns the correct name' do
+      expect(genre.name).to eq('Rock')
     end
-    
-    
-    describe '#add_item' do
-        it "adds an item to the items array" do
-        genre.add_item("punk")
-        expect(genre.items).to eq(["punk"])
-        end
+  end
+
+
+  describe '#add_item' do
+    it 'adds an item to the items array' do
+      genre.add_item('punk')
+      expect(genre.items).to eq(['punk'])
     end
-    end
+  end
+end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,17 +1,19 @@
 require './models/music_album'
 
 describe MusicAlbum do
-  let(:music_album) { MusicAlbum.new(genre: "Rock", author: "The Beatles", label: "Apple", publish_date: "1968-11-22", on_spotify: true) }
-    
+  let(:music_album) do
+    MusicAlbum.new(genre: 'Rock', author: 'The Beatles', label: 'Apple', publish_date: '1968-11-22', on_spotify: true)
+  end
+
   describe '#new' do
-    it "returns a new MusicAlbum object" do
+    it 'returns a new MusicAlbum object' do
       expect(music_album).to be_an_instance_of(MusicAlbum)
     end
   end
 
   describe '#genre' do
-    it "returns the correct genre" do
-      expect(music_album.genre).to eq("Rock")
+    it 'returns the correct genre' do
+      expect(music_album.genre).to eq('Rock')
     end
   end
 
@@ -21,20 +23,20 @@ describe MusicAlbum do
     end
   end
 
-  describe "#can_be_archived?" do
-    context "when the album is on Spotify and can be archived" do
-      it "returns true" do
+  describe '#can_be_archived?' do
+    context 'when the album is on Spotify and can be archived' do
+      it 'returns true' do
         music_album.on_spotify = true
         allow(music_album).to receive(:super).and_return(true)
         expect(music_album.can_be_archived?).to eq(true)
       end
     end
-    context "when the album is not on Spotify and can be archived" do
-        it "returns false" do
-            music_album.on_spotify = false
-            allow(music_album).to receive(:super).and_return(true)
-            expect(music_album.can_be_archived?).to eq(false)
-        end
+    context 'when the album is not on Spotify and can be archived' do
+      it 'returns false' do
+        music_album.on_spotify = false
+        allow(music_album).to receive(:super).and_return(true)
+        expect(music_album.can_be_archived?).to eq(false)
+      end
     end
   end
 end
