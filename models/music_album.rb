4681fa11d6b304c79c6399e4f984494d20619bb4
Daniela Moreno <dani9292@hotmail.com>
2023-07-04 11:43:54 -0500

    Add properties and methods to Music Album class

--- models/music_album.rb
+++ models/music_album.rb
@@ -2,8 +2,15 @@ class Music_album < Item
 
     def initialize(genre, author, label, publish_date, archived: false, on_spotify: false)
         super(genre, author, label, publish_date, archived: false)
-        @artist = artist
-        @genre = genre
-        @year = year
+        @on_spotify = on_spotify
+        @archived = archived
+    end
+
+    def can_be_archived?
+        if @archived == true
+            return true
+        else
+            return false
+        end
     end
 end
