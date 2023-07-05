30d6bf4984c4a45a75cec5c9db7f99b679f40623
Daniela Moreno <dani9292@hotmail.com>
2023-07-05 15:26:51 -0500

    Update logic for album music

--- managers/music_album_manager.rb
+++ managers/music_album_manager.rb
@@ -1,5 +1,8 @@
+require 'json'
 require './models/music_album'
 require './models/genre'
+require './models/author'
+require './models/label'
 
 class MusicAlbumManager
   def initialize
@@ -11,8 +14,8 @@ class MusicAlbumManager
     @music_albums.each_with_index do |music_album, index|
       puts "#{index + 1}. Music Album #{index + 1}:"
       puts "  Genre: #{music_album.genre.name}"
-      puts "  Author: #{music_album.author}"
-      puts "  Label: #{music_album.label}"
+      puts "  Author: #{music_album.author.first_name} #{music_album.author.last_name}"
+      puts "  Label: #{music_album.label.title}"
       #puts "  Published Date: #{music_album.publish_date}"
       puts "  On Spotify: #{music_album.on_spotify}"
       puts "------------"
@@ -26,14 +29,22 @@ class MusicAlbumManager
   end
 
   def add_music_album
+
+    puts 'Enter the name of author of the music album:'
+    name_author = gets.chomp
+
+    puts 'Enter the last name of author of the music album:'
+    last_name_author = gets.chomp
+
+    puts 'Enter the title of the music album:'
+    title = gets.chomp
+
     puts 'Enter the genre of the music album:'
     genre = gets.chomp
-    puts 'Enter the author of the music album:'
-    author = gets.chomp
-    puts 'Enter the label of the music album:'
-    label = gets.chomp
+   
     puts 'Enter the published date of the music album (YYYY-MM-DD):'
     publish_date = gets.chomp
+
     puts 'Enter if it\'s on Spotify (y/n):'
     on_spotify_input = gets.chomp.downcase
 
@@ -41,16 +52,39 @@ class MusicAlbumManager
 
     options = {
       genre: Genre.new(genre),
-      author: author,
-      label: label,
+      author: Author.new(name_author, last_name_author),
+      label: Label.new(title, color: nil),
       publish_date: publish_date,
       on_spotify: on_spotify
     }
 
     genre_obj = options[:genre]
     @genres << genre_obj unless @genres.include?(genre_obj)
-    puts 'Music album was added successfully!'
+   
     music_album = MusicAlbum.new(options)
     @music_albums << music_album
+
+    puts 'Music album was added successfully!'
+    #write_to_json
+  end
+
+  def write_to_json
+    File.open('music_albums.json', 'w') do |file|
+      file.write(JSON.pretty_generate(@music_albums.map(&:to_hash)))
+    end
+
+    puts 'Music albums data written to music_albums.json successfully!'
+  end
+
+  def to_hash
+    {
+        id: @id,
+        genre: @genre.to_hash,
+        author: @author,
+        label: @label,
+        publish_date: @publish_date.to_s,
+        archived: @archived,
+        on_spotify: @on_spotify
+    }
   end
 end
