21701c5dce9f8d8d4f311c0f9cdc13960ef8e75f
christianonoh <onohchibyk@gmail.com>
2023-07-05 22:47:45 +0100

    Initialize BookManager

--- managers/book_manager.rb
+++ managers/book_manager.rb
@@ -1,2 +1,13 @@
+require 'json'
+require './models/book'
+require './models/genre'
+require './models/author'
+require './models/label'
+
+
 class BookManager
+  def initialize
+    @books = []
+    @labels = []
+  end
 end
