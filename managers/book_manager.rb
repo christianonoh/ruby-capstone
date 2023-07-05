6e604e6596bbf60e7688928ecccd6ecabd133fed
christianonoh <onohchibyk@gmail.com>
2023-07-05 22:48:12 +0100

    Add add_book method

--- managers/book_manager.rb
+++ managers/book_manager.rb
@@ -10,4 +10,37 @@ class BookManager
     @books = []
     @labels = []
   end
+
+  def add_book
+    print 'Enter book title: '
+    title = gets.chomp
+    print 'Enter first name of author: '
+    author_first_name = gets.chomp
+    print 'Enter last name of author: '
+    author_last_name = gets.chomp
+    print 'Enter book genre: '
+    genre = gets.chomp
+    print 'Enter the published date (YYYY-MM-DD): '
+    publish_date = gets.chomp
+    print 'Enter book color: '
+    color = gets.chomp
+    print 'Enter book publisher: '
+    publisher = gets.chomp
+    print 'Is the book cover state "Good" or "Bad"?: '
+    cover_state = gets.chomp
+
+    options = {
+      genre: Genre.new(genre),
+      author: Author.new(author_first_name, author_last_name),
+      label: Label.new(title, color: nil),
+      publish_date: publish_date,
+      publisher: publisher,
+      cover_state: cover_state
+    }
+    label_obj = options[:label]
+    @labels << label_obj unless @labels.include?(label_obj)
+    puts "#{title} by #{author_first_name + ' ' + author_last_name} was added successfully!"
+    book = Book.new(options)
+    @books << book
+  end
 end
