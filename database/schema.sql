bc20c9162201fe9c66467dd17bceee01cf2ca244
julzedz <edoziechukwuma@gmail.com>
2023-07-06 16:45:16 +0100

    Create Author table

--- database/schema.sql
+++ database/schema.sql
@@ -6,6 +6,10 @@
 
 
 /* Create Table for Game and Author & Join table */
-
+CREATE TABLE Author(
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL
+)
 
 /* Create Table for Music and Genre & Join table */
