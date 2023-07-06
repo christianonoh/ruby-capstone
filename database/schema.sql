2177fb567382f258617323c9cd5b164eb1898c78
Daniela Moreno <dani9292@hotmail.com>
2023-07-06 10:17:32 -0500

    Create comments

---
+++ database/schema.sql
@@ -0,0 +1,11 @@
+
+/* Create Database */
+
+
+/* Create Table for Book and label & Join table */
+
+
+/* Create Table for Game and Author & Join table */
+
+
+/* Create Table for Music and Genre & Join table */
